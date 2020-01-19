6b35de46a29c194828480ac5d0e1fb74685152e7
Thiago Nascimento <tdnascdom@gmail.com>
2020-01-19 16:45:02 -0300

    Criando obejetos e configurações de persistencia

---
+++ src/main/java/br/com/compra/cr/TesteJPA.java
@@ -0,0 +1,33 @@
+package br.com.compra.cr;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.compra.cr.dados.produto.Produto;
+
+@SpringBootApplication
+public class TesteJPA {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompraRapidaApplication.class, args);
+		Produto p1 = new Produto(null, 123456, "Mouse", "informatica");
+		Produto p2 = new Produto(null, 123456, "Mouse", "informatica");
+		Produto p3 = new Produto(null, 123456, "Mouse", "informatica");
+		Produto p4 = new Produto(null, 123456, "Mouse", "informatica");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("data-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.persist(p4);
+		em.getTransaction().commit();
+		System.out.println("pronto!");
+	}
+
+}
---
+++ src/main/java/br/com/compra/cr/dados/produto/Produto.java
@@ -0,0 +1,77 @@
+package br.com.compra.cr.dados.produto;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private Integer codigoProduto;
+	private String nomeProduto;
+	private String descricaoProduto;
+
+	public Produto() {
+	}
+
+	public Produto(Integer id, Integer codigoProduto, String nomeProduto, String descricaoProduto) {
+		super();
+		this.id = id;
+		this.codigoProduto = codigoProduto;
+		this.nomeProduto = nomeProduto;
+		this.descricaoProduto = descricaoProduto;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getCodigoProduto() {
+		return codigoProduto;
+	}
+
+	public void setCodigoProduto(Integer codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", codigoProduto=" + codigoProduto + ", nomeProduto=" + nomeProduto
+				+ ", descricaoProduto=" + descricaoProduto + "]";
+	}
+
+}
