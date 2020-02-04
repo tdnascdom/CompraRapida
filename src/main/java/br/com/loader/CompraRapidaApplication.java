3d5377206e307ebf3212b2ae930b7180d2bca0ff
Thiago Nascimento <tdnascdom@gmail.com>
2020-02-04 19:59:59 -0300

    Criando oepração de instanciação

--- src/main/java/br/com/loader/CompraRapidaApplication.java
+++ src/main/java/br/com/loader/CompraRapidaApplication.java
@@ -1,13 +1,32 @@
 package br.com.loader;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.loader.domain.Categoria;
+import br.com.loader.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CompraRapidaApplication {
+public class CompraRapidaApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompraRapidaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		repo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
